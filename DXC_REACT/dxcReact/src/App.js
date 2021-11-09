044c66c65e5b5b2820c8851ddd2f36777d93c203
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-09 11:12:03 +0530

    counter increment button click

--- DXC_REACT/dxcReact/src/App.js
+++ DXC_REACT/dxcReact/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+
+      <Counter />
+
       <Message />
 
       <Greet name="Shaik" college="IIT">
