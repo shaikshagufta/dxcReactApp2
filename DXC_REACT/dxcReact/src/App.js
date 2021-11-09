5acd0aadfdba35eb8794b75e7aa528d9f645d04e
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-09 13:30:51 +0530

    event binding with arrow functions

--- DXC_REACT/dxcReact/src/App.js
+++ DXC_REACT/dxcReact/src/App.js
@@ -1,11 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
+
 import EventBind from './components/EventBind';
 
 function App() {
--- DXC_REACT/dxcReact/src/components/EventBind.js
+++ DXC_REACT/dxcReact/src/components/EventBind.js
@@ -18,11 +18,12 @@ class EventBind extends Component {
     }
 
     render() {
-        //using bind im tying clickhandler to this keyword
+        //using bind im tying clickhandler to this keyword -- binding in render method
         return (
             <div>
                 <div>{this.state.message}</div>
-                <button onClick={this.clickHandler.bind(this)}>click</button>
+                { /*<button onClick = {this.clickHandler.bind(this)}>click</button>  */}
+                <button onClick={() => this.clickHandler()}>click</button>
             </div>
         )
     }
