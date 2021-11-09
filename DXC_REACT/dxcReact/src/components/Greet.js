6b11e9860f875d7a7a3fb8b234fabc625d416632
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-09 11:18:10 +0530

    destructing props

--- DXC_REACT/dxcReact/src/components/Greet.js
+++ DXC_REACT/dxcReact/src/components/Greet.js
@@ -1,17 +1,17 @@
 import react from 'react'
-import App from '../App'
 
 /*function Greet(){
    return <h1>welcome shaggu</h1>
 }*/
 
-const Greet = (props) => {
-   console.log(props);
+//const Greet = ({name,college}) => {
+const Greet = props => {
+   const { name, college } = props
+   //console.log(props);
    //props.name = 'dxc'
    return (
       <div>
-         <h1> hello  {props.name} you are from {props.college}</h1>
-         {props.children}
+         <h1> hello  {name} you are from {college}</h1>
       </div>
    )
 }
