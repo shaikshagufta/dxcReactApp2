fa84075340514e7426666467af3d9cc24f1327a5
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-09 11:48:59 +0530

    event handling

--- DXC_REACT/dxcReact/src/App.js
+++ DXC_REACT/dxcReact/src/App.js
@@ -4,10 +4,14 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <ClassClick />
+      <FunctionClick />
 
       <Counter />
 
