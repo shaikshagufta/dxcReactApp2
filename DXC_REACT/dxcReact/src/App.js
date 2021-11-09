78a1e61614df1a5d87ca0c9fef058dbba4430d41
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-09 12:38:32 +0530

    event binding

--- DXC_REACT/dxcReact/src/App.js
+++ DXC_REACT/dxcReact/src/App.js
@@ -6,26 +6,12 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <ClassClick />
-      <FunctionClick />
-
-      <Counter />
-
-      <Message />
-
-      <Greet name="Shaik" college="IIT">
-        <p> this is children property</p>
-      </Greet>
-      <Greet name="Shaik" college="Oxford">
-        <button>click me</button>
-      </Greet>
-      <Greet />
-
-      <Welcome name="Shagufta" alias="Shaggu" />
+      <EventBind />
 
     </div>
   );
