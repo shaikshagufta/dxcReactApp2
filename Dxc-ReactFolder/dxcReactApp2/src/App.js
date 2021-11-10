b331124fb7fafc83eedd599d833399ac87ce8b58
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 15:34:21 +0530

    counter increment button click

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -10,15 +11,17 @@ function App() {
     <div className="App">
       <Message />
 
+      <Counter />
+
       <Greet name="Shaggu" college="IIT">
         <p> this is children property</p>
       </Greet>
       <Greet name="Shayi" college="Oxford" >
-      <button>click me</button>
+        <button>click me</button>
       </Greet >
       <Greet name="Shaik" />
 
-      <Welcome name ="Shagufta" alias = "Shaggu"/>
+      <Welcome name="Shagufta" alias="Shaggu" />
     </div>
   );
 }
