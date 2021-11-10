874baceeec859ae9590eb86aa72f209de02ce063
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 23:24:23 +0530

    added a comment box to form

--- Dxc-ReactFolder/dxcReactApp2/src/components/Form.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/Form.js
@@ -3,24 +3,29 @@ import React, { Component } from 'react'
 export class Form extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             username: '',
-             comments: ''
+            username: '',
+            comments: ''
         }
     }
-    /*handleUsernameChange = (event) => {
+    handleUsernameChange = (event) => {
         this.setState({
             username: event.target.value
         })
-    }*/
-//for app to react to the text we added a handler
+    }
+    //for app to react to the text we added a handler for username &
+    //added another div for commets too with a label but the text is not hadled yet
     render() {
         return (
             <form>
                 <div>
                     <label>username</label>
-                    <input type='text' />
+                    <input type='text' value={this.state.username} onChange={this.handleUsernameChange} />
+                </div>
+                <div>
+                    <label>comments</label>
+                    <textarea value={this.state.comments}></textarea>
                 </div>
 
             </form>
