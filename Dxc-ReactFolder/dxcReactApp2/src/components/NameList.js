1d824526ba80143e8d3bd697093c73657fcaad83
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 19:00:43 +0530

    list of persons json with multiple properties

--- Dxc-ReactFolder/dxcReactApp2/src/components/NameList.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/NameList.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Shaggu', 'Shayi', 'Sony', 'Moin', 'React']
-    const nameList = names.map(name => <h2>{name}</h2>)
+    /*     const names = ['Shaggu', 'Shayi', 'Sony', 'Moin', 'React']
+        const nameList = names.map(name => <h2>{name}</h2>) */
+    const persons = [
+        {
+            id: 1,
+            name: 'Shagufta',
+            age: 21,
+            skill: 'react'
+        },
+        {
+            id: 2,
+            name: 'Shayasta',
+            age: 21,
+            skill: 'MuleSoft'
+        },
+        {
+            id: 3,
+            name: 'Monisha',
+            age: 20,
+            skill: 'Java'
+        },
+        {
+            id: 4,
+            name: 'Moinuddin',
+            age: 24,
+            skill: 'Spring'
+        }
+    ]
+    const personList = persons.map(person => <h2>I am {person.name}, I am {person.age} years old and I am skilled in {person.skill}</h2>)
     return (
-        <div>{nameList}</div>
+        <div>{personList}</div>
     )
 }
 
