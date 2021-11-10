ab6146b967abb43646303e2100d7eb8b4f72f658
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 15:50:50 +0530

    destructing staeful components //

--- Dxc-ReactFolder/dxcReactApp2/src/components/Welcome.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/Welcome.js
@@ -4,7 +4,7 @@ export class Welcome extends Component {
 
     render() {
         const { name, alias } = this.props
-        const { state1, state2 } = this.state
+        //const { state1, state2 } = this.state
         return <h1>welcome ms {name} your alias is {alias}</h1>
     }
 }
