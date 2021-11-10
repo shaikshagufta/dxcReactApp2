eca91e405837cabc8f7c23e49ef394ffd9f0f161
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 15:49:21 +0530

    destructing stateful component

--- Dxc-ReactFolder/dxcReactApp2/src/components/Welcome.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/Welcome.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export class Welcome extends Component {
 
     render() {
-        return <h1>welcome mr {this.props.name} your alias is {this.props.alias}</h1>
+        const { name, alias } = this.props
+        const { state1, state2 } = this.state
+        return <h1>welcome ms {name} your alias is {alias}</h1>
     }
 }
 
