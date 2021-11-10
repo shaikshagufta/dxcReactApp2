7a1a332e18c6101930f849f30348e489f485ca48
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 16:21:09 +0530

    event handling

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -4,12 +4,17 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import ClassClick from './components/ClassClick';
+import FunctionClick from './components/FunctionClick';
 
 
 function App() {
   return (
     <div className="App">
       <Message />
+
+      <ClassClick />
+      <FunctionClick />
 
       <Counter />
 
---
+++ Dxc-ReactFolder/dxcReactApp2/src/components/ClassClick.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class ClassClick extends Component {
+    clickHandler(){
+        console.log('clicked from classclick')
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickHandler}>click me class</button>
+            </div>
+        )
+    }
+}
+
+export default ClassClick
