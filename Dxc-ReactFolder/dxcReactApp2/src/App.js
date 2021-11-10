103f731adbe481f5e0c3da78a44a32300eead829
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 17:57:02 +0530

    conditional rendering -- if/else

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -8,11 +8,14 @@ import ClassClick from './components/ClassClick';
 import FunctionClick from './components/FunctionClick';
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 
 
 function App() {
   return (
     <div className="App">
+
+      <UserGreeting />
       <ParentComponent />
       <EventBind />
     </div>
---
+++ Dxc-ReactFolder/dxcReactApp2/src/components/UserGreeting.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class UserGreeting extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            IsLoggedIn: true
+        }
+    }
+
+    render() {
+        if (this.state.IsLoggedIn) {
+            return <div>Welcome Shagufta</div>
+        }
+        else {
+            return <div>Welcome Guest</div>
+        }
+        /*return (
+            <div>
+               <div>Wlcome Shagufta</div>
+               <div>Welcome Guest</div>
+
+            </div>
+        )*/
+    }
+}
+
+export default UserGreeting
