f88c318d96a0f6401b670383c89571453c905a79
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 18:19:13 +0530

    map using a const nameslist

--- Dxc-ReactFolder/dxcReactApp2/src/components/NameList.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/NameList.js
@@ -4,9 +4,9 @@ function NameList() {
     const names = ['Shaggu', 'Shayi', 'Sony']
     return (
         <div>
-            <h2>{names[0]}</h2>
-            <h2>{names[1]}</h2>
-            <h2>{names[2]}</h2>
+
+            {names.map(name => <h2>{name}</h2>)}
+
         </div>
     )
 }
