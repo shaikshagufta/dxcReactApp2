2e104e8f5facc354cb0cfab55862065751d1d26e
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 16:54:49 +0530

    event binding

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -6,27 +6,13 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import ClassClick from './components/ClassClick';
 import FunctionClick from './components/FunctionClick';
+import EventBind from './components/EventBind';
 
 
 function App() {
   return (
     <div className="App">
-      <Message />
-
-      <ClassClick />
-      <FunctionClick />
-
-      <Counter />
-
-      <Greet name="Shaggu" college="IIT">
-        <p> this is children property</p>
-      </Greet>
-      <Greet name="Shayi" college="Oxford" >
-        <button>click me</button>
-      </Greet >
-      <Greet name="Shaik" />
-
-      <Welcome name="Shagufta" alias="Shaggu" />
+      <EventBind />
     </div>
   );
 }
---
+++ Dxc-ReactFolder/dxcReactApp2/src/components/EventBind.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export class EventBind extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            message: 'Hello'
+        }
+    }
+
+    clickHandler() {
+        console.log(this)
+        this.setState({
+            message: 'good bye'
+        })
+    }
+
+
+    render() {
+        //using bind im tying clickhandler to this keyword
+        return (
+            <div>
+                <div>{this.state.message}</div>
+                <button onClick={this.clickHandler.bind(this)}>click</button>
+            </div>
+        )
+    }
+}
+
+
+export default EventBind
