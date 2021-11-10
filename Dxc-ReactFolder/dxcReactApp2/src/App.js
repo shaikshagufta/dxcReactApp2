b13b9c7f15262948824a4ea9eca381cc99933c1f
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-10 18:16:11 +0530

    name lists

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -9,12 +9,13 @@ import FunctionClick from './components/FunctionClick';
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 
 
 function App() {
   return (
     <div className="App">
-
+      <NameList />
       <UserGreeting />
       <ParentComponent />
       <EventBind />
