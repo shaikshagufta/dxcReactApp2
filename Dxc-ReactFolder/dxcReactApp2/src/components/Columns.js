b2d148de888e0d77f1e73f3dbe5ca305ac489721
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-11 12:38:16 +0530

    fragments in a table step 2

--- Dxc-ReactFolder/dxcReactApp2/src/components/Columns.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/Columns.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+//added fragments here to observe the console in inspect of app showing no warnings
 function Columns() {
     return (
-        <div>
+        <React.Fragment>
             <td>Name</td>
             <td>Shaggu</td>
-        </div>
+        </React.Fragment>
     )
 }
 
