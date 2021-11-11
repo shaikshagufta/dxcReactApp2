9d36e926356cf1e324bf15043959561ff7ec914d
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-11 12:57:23 +0530

    fragments in a table final

--- Dxc-ReactFolder/dxcReactApp2/src/components/Columns.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/Columns.js
@@ -1,11 +1,39 @@
 import React from 'react'
 //added fragments here to observe the console in inspect of app showing no warnings
 function Columns() {
+    // we should create an array od objects
+    const items = [
+        {
+            title: 'Shagufta',
+            id: 1,
+            description: 'Me'
+        },
+        {
+            title: 'Shayasta',
+            id: 2,
+            description: 'Twin'
+
+        },
+        {
+            title: 'Sony',
+            id: 3,
+            description: 'Nephew'
+
+        },
+    ]
     return (
         <React.Fragment>
-            <td>Name</td>
-            <td>Shaggu</td>
-        </React.Fragment>
+            {
+                items.map(
+                    item => (
+                        <React.Fragment key={item.id}>
+                            <h1>{item.title}</h1>
+                            <p>{item.description}</p>
+                        </React.Fragment>
+                    )
+                )
+            }
+            </React.Fragment>
     )
 }
 
