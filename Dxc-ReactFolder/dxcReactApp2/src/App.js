d3745ec99f2208959f35562ac07278fdffc5cec7
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-17 16:30:50 +0530

    run effects only once using hook

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import './appStyles.css';
 import ClassMouse from './components/hooks1/useeffect/ClassMouse';
+import HookMouse from './components/hooks1/useeffect/HookMouse';
 
 
 
 function App() {
   return (
     <div className="App">
-      <ClassMouse />
+      <HookMouse />
       
     </div>
   );
