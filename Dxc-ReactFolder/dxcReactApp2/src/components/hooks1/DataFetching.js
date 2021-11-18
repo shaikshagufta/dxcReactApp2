4b1faee6411b65bcca2b11cb8d29e8d335fc0410
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-18 12:36:22 +0530

    fetching the post onclick of a button

--- Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/DataFetching.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/DataFetching.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 function DataFetching() {
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [idFromButtonClick,setIdFromButtonClick] = useState(1)
     /**
     * ///useEffect is about the lifecycle methods of a class component --
     *  if you want to use lifecycle methods of a class component in a stateless functional component --then we use useEffect hook
@@ -11,7 +12,7 @@ function DataFetching() {
     useEffect(
         //the arrow function below will get executed after every render
         () => {
-            axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
                 .then(res => {
                     console.log(res)
                     setPost(res.data)
@@ -20,9 +21,14 @@ function DataFetching() {
                     console.log(err)
                 })
         },
-        [id] //run this useEffect hook only once ie after component did mount
-        //we added id in it to say- mount everytime the id changes 
+        [idFromButtonClick] //run this useEffect hook only once ie after component did mount
+        //we added idFromButtonClick in it to say- mount everytime the button is clicked 
     )
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
+
 
     return (
         <div>
@@ -31,6 +37,7 @@ function DataFetching() {
                 value={id}
                 onChange={e => setId(e.target.value)}
             />
+            <button type = "button" onClick = {handleClick}> fetch post</button>
             <div>{post.title}</div>
             {/*<ul>
                 {
