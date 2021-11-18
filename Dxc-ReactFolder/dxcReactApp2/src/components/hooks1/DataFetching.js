6f94161b563e5fc32527c05b620bd81d6c0694b1
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-18 11:53:49 +0530

    getting the data only once by passing [ ] empty array to useEffect hook

--- Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/DataFetching.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/DataFetching.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 function DataFetching() {
     const [posts, setPosts] = useState([])
+    /**
+    * ///useEffect is about the lifecycle methods of a class component --
+    *  if you want to use lifecycle methods of a class component in a stateless functional component --then we use useEffect hook
+    */
     //Promise -- Deferred obj in kotlin--
     useEffect(
+        //the arrow function below will get executed after every render
         () => {
             axios.get('https://jsonplaceholder.typicode.com/posts')
                 .then(res => {
@@ -13,7 +18,8 @@ function DataFetching() {
                 .catch(err => {
                     console.log(err)
                 })
-        }
+        },
+        [] //run this useEffect hook only once ie after component did mount
     )
 
     return (
