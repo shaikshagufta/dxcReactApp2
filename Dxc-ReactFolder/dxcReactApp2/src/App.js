1487957fe6a1a4ec54bf4ca77d3afb8b361ebab0
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-18 11:49:11 +0530

    posts all using setPosts DataFetching

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './appStyles.css';
+import DataFetching from './components/hooks1/DataFetching';
 import ClassMouse from './components/hooks1/useeffect/ClassMouse';
 import HookMouse from './components/hooks1/useeffect/HookMouse';
 import MouseContainer from './components/hooks1/useeffect/MouseContainer';
@@ -9,7 +10,7 @@ import MouseContainer from './components/hooks1/useeffect/MouseContainer';
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      <DataFetching />
       
     </div>
   );
