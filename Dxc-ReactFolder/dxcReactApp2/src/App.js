8b83451bfdd2dbb1ce99b0ca4f821b079bfaaf3b
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 15:45:59 +0530

    operating App using CompA using useContext andReducer

--- Dxc-ReactFolder/dxcReactApp2/src/App.js
+++ Dxc-ReactFolder/dxcReactApp2/src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 import './App.css';
 import './appStyles.css';
 import ComponentA from './components/hooks1/contexthook/ComponentA';
+import CompA from './components/hooks1/contextNreducer/CompA';
+import CompB from './components/hooks1/contextNreducer/CompB';
+import CompC from './components/hooks1/contextNreducer/CompC';
 import CounterOne from './components/hooks1/reducerhook/CounterOne';
 import CounterThree from './components/hooks1/reducerhook/CounterThree';
 import CounterTwo from './components/hooks1/reducerhook/CounterTwo';
@@ -11,13 +14,37 @@ import CounterTwo from './components/hooks1/reducerhook/CounterTwo';
 export const UserContext = React.createContext()    //exporting a context
 export const BatchContext = React.createContext()
 
-function App() {
-  return (
-    <div className="App">
 
-      <CounterThree />
+export const CountContext = React.createContext()
+const initialState = 0
+
+const reducer = (state,action) => {
+  switch(action){
+      case 'increment': return state+1
+      case 'decrement': return state-1
+      case 'reset': return initialState
+      default: return state
+  }
+}
+
+function App() {
+  const [count,dispatch] = useReducer(reducer,initialState)// defining const and dispatch
+  return (
+    // embedding in a provider passing an object as value 
+    <CountContext.Provider
+    value = {  
+      {countState: count, countDispatch: dispatch} //object
+     }>
+    
+    <div className="App">
+    Count in app.js is {count}
+
+      <CompA />
+      <CompB />
+      <CompC />
 
     </div>
+    </CountContext.Provider>
   );
 }
 
---
+++ Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/contextNreducer/CompD.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompD() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CompD
