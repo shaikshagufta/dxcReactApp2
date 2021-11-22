8ea316d16057afad62d5c914e1ae4cb45291169f
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-23 00:59:33 +0530

    pulling transactions to IEComp & add calculations

--- EXPENCE_TRACKER_REACT/t4reactapp/src/components/IncomeExpenses.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/components/IncomeExpenses.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const IncomeExpenses = () => {
+    //pulling the transactions
+    const { transactions } = useContext(GlobalContext);
+    /*
+    cause each transaction is an object with 'id,text,ammount' 
+    we map through and get all amounts into an array
+    */
+    const amounts = transactions.map(transaction => transaction.amount);
+    /*
+    For income we,
+    filter through and get positive numbers
+    add them all together using reduce 
+    and add a limit to decimal using toFix
+    */
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    /*
+    For expence we,
+    filter through and get negative numbers
+    add them all together using reduce 
+    and add a limit to decimal using toFix
+    */
+    const expense = (
+        amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+        -1
+    ).toFixed(2);
+    // outputing income and expenses
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">{income}</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p className="money minus">-$0.00</p>
+                <p className="money minus">{expense}</p>
             </div>
         </div>
     )
