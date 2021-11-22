5756b0088bd7884691af44af7459d5a387fcca19
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-23 00:29:25 +0530

    adding functionality  - border color

--- EXPENCE_TRACKER_REACT/t4reactapp/src/components/Transaction.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/components/Transaction.js
@@ -4,9 +4,10 @@ export const Transaction = ({ transaction }) => {
     //handling the ammount 
     const sign = transaction.amount < 0 ? '-' : '+';
     return (
-
-        <li className="minus">
+//handling border by making the className dynamic 
+        <li className={transaction.amount <0 ? 'minus' : 'plus'}>
             {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
         </li>
     )
 }
+//later on we shall add a delete action and add that functionality too
--- EXPENCE_TRACKER_REACT/t4reactapp/src/components/TransactionList.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/components/TransactionList.js
@@ -4,7 +4,7 @@ import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
     const { transactions } = useContext(GlobalContext);
-    // Now the list is going to be replaced by the Transaction component
+
     return (
         <>
             <h3>History</h3>
@@ -14,8 +14,3 @@ export const TransactionList = () => {
         </>
     )
 }
-/*
-bringing{ transactions } from Global state
-mapping through- for eact transaction we render a Transaction component by passing a prop
-this prop in this maping is called a list and this list needs a key wich needs to be unique
-*/
