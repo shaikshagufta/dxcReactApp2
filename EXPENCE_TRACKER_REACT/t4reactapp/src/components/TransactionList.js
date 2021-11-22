4d109b8de43b4789401b3735304039667dec4f56
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 23:21:30 +0530

    outupt the list of transactions in the app

--- EXPENCE_TRACKER_REACT/t4reactapp/src/components/TransactionList.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/components/TransactionList.js
@@ -1,19 +1,27 @@
-import React,{useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import { GlobalContext } from '../components/context/GlobalState';
 
 export const TransactionList = () => {
-const context = useContext(GlobalContext);
-
- console.log(context)// to check the log if pulling is posible
-
+    const { transactions } = useContext(GlobalContext);
+    //insted of "console.log(context)" we can use destructuring like above// to check the log if pulling is posible
+    /* 
+    here transactions is an array so we map through it and output each transaction 
+    this we shall do it in a different component 
+    but for now we add an expression as follows
+    */
     return (
         // later on the list is going to be replaced by the Transaction component
         <div>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
+                {transactions.map(transactions => (
+                    <li className="minus">
+                        {transactions.text} <span>-$400</span><button className="delete-btn">x</button>
+                    </li>//becoz now we have the access to transaction values here
+                ))}
+                {/* <li className="minus">
                     Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                </li> */}
             </ul>
         </div>
     )
