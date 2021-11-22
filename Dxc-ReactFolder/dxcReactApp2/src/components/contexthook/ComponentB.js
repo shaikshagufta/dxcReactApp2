bdc8256b3c1cf7ffcd2eb7810d0d6599c6906e6a
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 10:13:57 +0530

    useContext hook to pass the data around

--- Dxc-ReactFolder/dxcReactApp2/src/components/contexthook/ComponentB.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/contexthook/ComponentB.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ComponentC from './ComponentC'
+import ComponentE from './ComponentE'
 
 function ComponentB() {
     return (
         <div>
-         <ComponentC /> 
+         <ComponentE /> 
         </div>
     )
 }
