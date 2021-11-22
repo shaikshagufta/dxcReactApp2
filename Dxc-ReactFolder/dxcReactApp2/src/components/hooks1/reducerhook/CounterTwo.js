f0970dabcedfc1d0fb9ff0c60458c454e6c54528
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 11:48:40 +0530

    managing multiple states within an object

--- Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/reducerhook/CounterTwo.js
+++ Dxc-ReactFolder/dxcReactApp2/src/components/hooks1/reducerhook/CounterTwo.js
@@ -2,13 +2,18 @@ import React, { useReducer } from 'react'
 //demo about state as an object
 
 const initialState = {  //currently this object has only one partition called firstCounter
-    firstCounter: 0
+    firstCounter: 0,
+    secondCounter: 10
 }
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'increment': return { firstCounter: state.firstCounter + action.value }
-        case 'decrement': return { firstCounter: state.firstCounter - action.value }
+        case 'increment': return {...state, firstCounter: state.firstCounter + action.value } //...state -> spread operator will give you the previous state of the object
+        case 'decrement': return {...state, firstCounter: state.firstCounter - action.value}
+
+        case 'increment2': return { ...state,secondCounter: state.secondCounter + action.value }
+        case 'decrement2': return { ...state, secondCounter: state.secondCounter - action.value}
+
         case 'reset': return initialState
         default: return state
     }
@@ -19,7 +24,8 @@ function CounterTwo() {
 
     return (
         <div>
-            <div> Count - {count.firstCounter}</div>
+            <div> Count1 - {count.firstCounter}</div>
+            <div> Count2 - {count.secondCounter}</div>
             <button
                 onClick={
                     () => dispatch({ type: 'increment', value: 1 })
@@ -42,7 +48,14 @@ function CounterTwo() {
                     () => dispatch({ type: 'reset' })
                 }> Reset</button>
 
-
+            <button
+                onClick={
+                    () => dispatch({ type: 'increment2', value:1 })
+                }> Increment2</button>
+            <button
+                onClick={
+                    () => dispatch({ type: 'decrement2' , value:1})
+                }> Decrement2</button>    
 
         </div>
     )
