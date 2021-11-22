51dec708bcfb6406b5c9ec3c868bd429dc10c299
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-23 00:46:26 +0530

    pulling transaction list to BalanceComp & adding calculations

--- EXPENCE_TRACKER_REACT/t4reactapp/src/components/Balance.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/components/Balance.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
-    return (// building the UI for Balance
-        //fragment
+    //pulling the transactions
+    const { transactions } = useContext(GlobalContext);
+/*
+cause each transaction is an object with 'id,text,ammount' 
+we map through and get all amounts into an array
+*/
+    const amounts = transactions.map(transaction => transaction.amount);
+//to calculate the total ammount -we use reduce to add the ammounts together and
+//and we use toFixed to- get 2 decimal places
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  
+    return (
+        // outputing the total
         <>
             <h4>Your Balance</h4>
-            <h1>$0.00</h1>
+            <h1>${total}</h1>
         </>
     )
 }
