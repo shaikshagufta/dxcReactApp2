0e756142931d8faf3a351360f2f28d6bf9e2200e
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 20:12:42 +0530

    texting TransactionList using console log

--- EXPENCE_TRACKER_REACT/t4reactapp/src/components/TransactionList.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/components/TransactionList.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React,{useContext, useState} from 'react';
+import { GlobalContext } from '../components/context/GlobalState';
 
 export const TransactionList = () => {
+const context = useContext(GlobalContext);
+
+ console.log(context)// to check the log if pulling is posible
+
     return (
         // later on the list is going to be replaced by the Transaction component
         <div>
