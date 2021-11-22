1cbcf4f2263114b4834402eb9a34cfadd3f33b49
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 18:56:51 +0530

    building the UI - for TransactionList

--- EXPENCE_TRACKER_REACT/t4reactapp/src/App.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Balance } from './components/Balance';
 import { Header } from './components/Header';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Balance />
       <IncomeExpenses />
+      <TransactionList />
     </div>
   );
 }
