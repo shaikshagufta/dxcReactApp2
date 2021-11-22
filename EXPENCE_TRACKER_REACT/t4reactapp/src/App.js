ff84c74900c377b5a70cf658edc75b12457216f6
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-22 18:49:36 +0530

    building the UI - for Income expense boxes

--- EXPENCE_TRACKER_REACT/t4reactapp/src/App.js
+++ EXPENCE_TRACKER_REACT/t4reactapp/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Balance } from './components/Balance';
 import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
 
 function App() {
   return (
     <div>
       <Header />
       <Balance />
+      <IncomeExpenses />
     </div>
   );
 }
