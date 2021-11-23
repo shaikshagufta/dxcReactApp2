4189e8fc5b5bebbd1139b99ce50da2b0263d0bae
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-23 12:09:49 +0530

    files operations

--- NODEJS_T4/test.js
+++ NODEJS_T4/test.js
@@ -1,3 +1,10 @@
-const name = 'yoshi';
+/* const name = 'yoshi';
 
-console.log(name);
+console.log(name); */
+ 
+const greet =(name) =>{// taking a function with name greet and take in the parameter name
+    console.log(`hello ${name}`)//`hello${name}`- template string; ${name}- variables declaration in it 
+}
+
+greet ('shagufta');
+greet('team4')
