398002a55239a7b03b47e1099753ea9548e88610
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-23 13:31:23 +0530

    streams

--- NODEJS_T4/stream.js
+++ NODEJS_T4/stream.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8' });
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
-/* 
 readStream.on('data', chunk => {
 
     console.log('---- NEW CHUNK ----');
@@ -10,6 +9,5 @@ readStream.on('data', chunk => {
     writeStream.write('\nNEW CHUNK:\n');
     writeStream.write(chunk);
 });
-*/
 // piping
- readStream.pipe(writeStream);
+readStream.pipe(writeStream);
