46a7dbc31df4dbba488e99daf5fff27818ac67ae
shaikshagufta <shaikshagufta99@gmail.com>
2021-11-25 10:27:55 +0530

    reactrouterv5

--- SMS_React_Folder_T6/sms-t4-react/src/App.js
+++ SMS_React_Folder_T6/sms-t4-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListStudentComponent from './components/ListStudentComponent';
@@ -12,11 +12,11 @@ function App() {
       <Router>
           <HeaderComponent />
           <div className="container">
-            <Routes>
+            <Switch>
               <Route path="/" exact element={<ListStudentComponent />} />
               <Route path="/student" element={<ListStudentComponent />} />
-              {<Route path="/add-student" element={<CreateStudentComponent />} />}
-            </Routes>
+              <Route path="/add-student" element={<CreateStudentComponent />} />
+            </Switch>
           </div>
           <FooterComponent />
       </Router>
